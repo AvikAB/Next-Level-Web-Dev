b421ab2f369eda8e6969332560a4bd6ed852a7d3
Avik <119071924+AvikAB@users.noreply.github.com>
2026-08-26 13:02:05 +0600

    Add while loop examples for iterating over fruits

--- Basic JS/05. Loops.js
+++ Basic JS/05. Loops.js
@@ -13,3 +13,26 @@ let fruits = ["Apple", "Jack fruits", "Mango", "Guava", "Litchi"];
 for(let i=0; i<fruits.length; i++){
     console.log("Khabo:",fruits[i]);
 }
+
+
+// While loop:
+let i = 0;
+while(i<10){
+    console.log(i);
+    i++;
+}
+
+let i = 10;
+while(i>=0){
+    console.log(i);
+    i--;
+}
+
+// Example:
+let fruits = ["Apple", "Jack fruits", "Mango", "Guava", "Litchi"];
+
+let i = 0;
+while(i<fruits.length){
+    console.log("Khobo:", fruits[i]);
+    i++;
+}
