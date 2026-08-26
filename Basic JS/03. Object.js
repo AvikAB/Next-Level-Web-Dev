2b064f84741be1d34cadfddc4f599b059fa4b0d8
Avik <119071924+AvikAB@users.noreply.github.com>
2026-08-26 11:58:17 +0600

    Enhance Object.js with nested object examples
    Added examples for accessing nested object properties and logging entries.

--- Basic JS/03. Object.js
+++ Basic JS/03. Object.js
@@ -23,3 +23,15 @@ user.address = {   // update
 console.log(Object.entries(user));   // key-val pair prints
 console.log(Object.keys(user));      // keys prints
 console.log(Object.values(user));    // values prints
+
+// Nested Object:
+// console.log(user["address"]["city"]);
+// console.log(user["address"]["zipcode"]);
+// console.log(user.address.area);
+
+let entry = Object.entries(user);
+// console.log(entry);
+
+console.log(entry[1]);
+console.log(entry[1][0]);
+console.log(entry[1][1]);
