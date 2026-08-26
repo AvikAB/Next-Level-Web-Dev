66b3ea26664dca7b1f6fbcb9d4d9da199c76d579
Avik <119071924+AvikAB@users.noreply.github.com>
2026-08-26 12:57:57 +0600

    Update 05. Loops.js

--- Basic JS/05. Loops.js
+++ Basic JS/05. Loops.js
@@ -6,3 +6,10 @@ for(let i=0; i<10; i++){
 for(let i=5; i>=0; i--){
     console.log(`cnt: ${i}`);
 }
+
+//Example:
+let fruits = ["Apple", "Jack fruits", "Mango", "Guava", "Litchi"];
+
+for(let i=0; i<fruits.length; i++){
+    console.log("Khabo:",fruits[i]);
+}
