fb00a990cc733338038393fbfcc707606932fcc4
Avik <119071924+AvikAB@users.noreply.github.com>
2026-08-28 13:15:51 +0600

    Update 08. PS_with_Array.js

--- Basic JS/08. PS_with_Array.js
+++ Basic JS/08. PS_with_Array.js
@@ -5,3 +5,12 @@ for(let i=1; i<marks.length; i++){
     if(mx<marks[i]) mx = marks[i];
 }
 console.log(mx);
+
+// Find the sum and avg:
+let marks = [25, 87, 99, 67, 34, 23];
+let sum = 0;
+for(let i=0; i<marks.length; i++){
+    sum += marks[i];
+}
+console.log(sum);
+console.log(sum/marks.length);
